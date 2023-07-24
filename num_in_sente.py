86bee221dde89e355edbc13e3cddb8fb051f5b0f
anupriya <anupriyaa981@gmail.com>
2023-07-24 09:03:47 +0530

    add new

---
+++ num_in_sente.py
@@ -0,0 +1,21 @@
+wrd=input('enter the number in words:')
+if wrd>0 and wrd<=9:
+    if wrd=='one':
+        print('1')
+    if wrd=='two':
+        print('2')
+if wrd=='three':
+    print('3')
+if wrd=='four':
+    print("4")
+if wrd=='five':
+    print('5')
+if wrd==('six'):
+    print('6')
+if wrd==('seven'):
+    print('7')
+if wrd==('eight'):
+    print('8')
+if wrd=='nine':
+    print('9')
+
