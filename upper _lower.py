60cb2f3800b046ed0fe75e4af8bb9100c153e152
anupriya <anupriyaa981@gmail.com>
2023-07-25 09:05:42 +0530

    new add

---
+++ upper _lower.py
@@ -0,0 +1,29 @@
+case=input('enter the case:')
+sen=(input("enter the number in lower case:"))
+if case=='up_low':
+    print(sen.lower())
+if case=='low_up':
+   print(sen.upper())
+
+
+
+
+
+
+
+#                             convert lower to upper
+# text = 'G3Ek5 F0R gE3K5'
+# \\# lower() function to convert
+# \\# string to lower_case
+# print("\nConverted String:")
+# print(text.lower())
+
+
+
+                            
+
+
+
+
+
+
