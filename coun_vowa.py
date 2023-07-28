d9947908c0c8d2aea32a14c49236eb08d666edc7
anupriya <anupriyaa98@gmail.com>
2023-07-28 09:04:57 +0530

    add new

--- coun_vowa.py
+++ coun_vowa.py
@@ -1,21 +1,18 @@
-# vowel=['a','e','i','o','u']
-# stri=input('enter the string:')
-# for i in stri:
-#   if vowel in i:
-#     print('yes')
-# else:
-#   print('no')
+vowel=['a','e','i','o','u']
+stri=input('enter the string:')
+
+   
+
+  
 
 
 
+# str=input('enter the string:')
+# count=0
+# a1=[]
+# for i in str:
+#    if (i=='a') or (i=='e') or (i=='i') or (i=='o') or (i=='u'):
+#      a1.append(i)
+# print(len(a1))
 
-
-str=input('enter the string:')
-count=0
-a1=[]
-for i in str:
-   if (i=='a') or (i=='e') or (i=='i') or (i=='o') or (i=='u'):
-     a1.append(i)
-else:
-          print('no')
    
---
+++ infinity.py
@@ -0,0 +1,17 @@
+# s='*'
+# for i in s:
+#     if i>=0:
+#          print(i)
+
+
+
+
+
+
+
+
+
+
+a=list([1,2,3,4])
+print(a)
+print (type(a))
