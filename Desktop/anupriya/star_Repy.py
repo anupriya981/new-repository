0544d6777e46c25234e84fd9f31af973f6a7fafb
anupriya <anpriyaa981@gmail.com>
2023-08-04 09:26:14 +0530

    add new

--- Desktop/anupriya/star_Repy.py
+++ Desktop/anupriya/star_Repy.py
@@ -1,2 +1,25 @@
-limit=int(input('enter the limit:'))
-for i in range(0,range,):
+#           *
+#          **
+#         ***
+#        ****
+#       *****
+#      ******
+#                                      print  pattern
+  
+
+limit=int(input('enter the limit of row:'))
+for i in range(1,limit+1):
+    print(' '*(limit-i)+'*'*i)
+
+
+
+
+
+
+
+
+
+
+
+
+
---
+++ Desktop/anupriya/star_re_op.py
@@ -0,0 +1,3 @@
+limit=(int(input('enter the limit:')))
+for i in range(limit,0,-1):
+    print(" "*(limit-i)+'*'*i)
