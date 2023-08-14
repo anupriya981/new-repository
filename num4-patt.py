5d778bcfee2772f4de0dee6a2ce4644fc55d13cd
anupriya <anupriyaa981@gmail.com>
2023-08-14 09:13:57 +0530

    add new

--- num4-patt.py
+++ num4-patt.py
@@ -17,8 +17,8 @@
 
 limit=int(input('enter the limit:'))
 for i in range(1,limit+1):
-    for j in range(1,i):
-      if(j%2!=0): 
-    
-       print(j,end='')
+    for j in range(1,limit):
+       f=i+i-1
+  
+       print(f,end='')
     print()
---
+++ num6.patt.py
@@ -0,0 +1,11 @@
+lim=int(input('entere the row:'))
+current_no=1
+for i in range(1,lim):
+    for j in range(i):
+        print(current_no,end=' ')
+        current_no+=1  
+    print()
+         
+
+
+
