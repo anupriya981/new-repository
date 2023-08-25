105dcf4d598989600a6c17447445e5b3303aab6d
anupriya <anupriyaa981@gmail.com>
2023-08-25 09:45:43 +0530

    add new

---
+++ contro-statement/break.py
@@ -0,0 +1,10 @@
+# 1
+# 2
+# 3
+
+for i in range(1,6):
+    print(i)
+    if i==3:
+        break
+    
+    
---
+++ contro-statement/continue.py
@@ -0,0 +1,10 @@
+
+# 1
+# 2
+# 4
+# 5
+
+for i in range(1,6):
+    if i==3:
+        continue
+    print(i)
