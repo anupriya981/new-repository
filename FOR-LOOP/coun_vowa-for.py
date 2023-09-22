6ec0f629a10aec5e51eecb395dd3149b84653834
anupriya <anupriyaa981@gmail.com>
2023-09-22 09:22:21 +0530

    add new

--- FOR-LOOP/coun_vowa-for.py
+++ FOR-LOOP/coun_vowa-for.py
@@ -1,5 +1,5 @@
-vowel=['a','e','i','o','u']
-stri=input('enter the string:')
+# vowel=['a','e','i','o','u']
+# stri=input('enter the string:')
 
    
 
@@ -7,12 +7,12 @@ stri=input('enter the string:')
 
 
 
-# str=input('enter the string:')
+str=input('enter the string:')
 # count=0
-# a1=[]
-# for i in str:
-#    if (i=='a') or (i=='e') or (i=='i') or (i=='o') or (i=='u'):
-#      a1.append(i)
-# print(len(a1))
+a1=[]
+for i in str:
+   if (i=='a') or (i=='e') or (i=='i') or (i=='o') or (i=='u'):
+     a1.append(i)
+print(len(a1))
 
    
--- contro-statement/prime-bk.py
+++ contro-statement/prime-bk.py
@@ -1,21 +1,35 @@
-list=[]
-prime_num=[]
-n=1
-for num in range(1,6):
+# list=[]
+# prime_num=[]
+# n=1
+# for num in range(1,6):
+#     num=input('enter the value:')
+#     list.append(num)
+# num=[9,4,3,7,2]
+# for i in num:
+   
+#     if prime==0:       
+#             prime_num.append(i)
+#     else:
+#        break 
+# print(prime_num)
+
+
+
+
+
+
+number=1
+while number>=9:
     num=input('enter the value:')
-    list.append(num)
-for i in num:
-   for prime in range(2,int(i//2)+1):
-    if prime==0:       
-            prime_num.append(i)
-    else:
-       break 
-print(prime_num)
+    for i in range(1,num+1):
+      if i%number==0:
+         print("not prime")
+      else:
+         print('prime')
+      
 
-
-
-
-
+  
+      print(i)
 
 
 
