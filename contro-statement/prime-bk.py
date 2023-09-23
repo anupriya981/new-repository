0b4c7cc22b8830211d617c75f5de88bd3ff6874d
anupriya <anupriyaa981@gmail.com>
2023-09-23 09:14:36 +0530

    add new

--- contro-statement/prime-bk.py
+++ contro-statement/prime-bk.py
@@ -18,21 +18,19 @@
 
 
 
-number=1
-while number>=9:
-    num=input('enter the value:')
-    for i in range(1,num+1):
-      if i%number==0:
-         print("not prime")
-      else:
-         print('prime')
+
+num=int(input('enter the value:'))
+if num>1:
+   for i in range(2,num):
+      if num%i==0:
+       break
+print("not prime")
+#       else: 
+#          print('prime')
+            
+# else:
+#       print("number is less than 1") 
+
       
-
-  
-      print(i)
-
-
-
-
-
-#pending
+         
+   
