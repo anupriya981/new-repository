58ada99e07363376f51d7ff9fe314aa410d4e6f5
anupriya <anupriyaa981@gmail.com>
2023-09-25 09:48:11 +0530

    python module

---
+++ python_modules/main.py
@@ -0,0 +1,20 @@
+import master
+
+print('choice=1:sum\n choice2:product\n choice3:difference\n choice4:multiplication\nchoice5:squre')
+choice=int(input('enter the choice:'))
+
+if choice==1:
+    master.sum()
+   
+
+if choice==2:
+    master.product()
+
+if choice==3:
+    master.difference()
+
+if choice==4:
+    master.difference()
+
+if choice==5:
+    master.squre()
