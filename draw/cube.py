dfa37cfbd30f0fa1914787e1039017748c54016f
anupriya <anupriyaa981@gmail.com>
2023-10-30 09:40:04 +0530

    add new

---
+++ draw/cube.py
@@ -0,0 +1,44 @@
+import turtle
+tk=turtle.Turtle()
+for i in range(2):
+    tk.right(90)
+    tk.forward(180)
+    tk.right(90)
+    tk.forward(180)
+
+
+
+tk.left(60)
+tk.forward(60)
+tk.left(30)
+tk.left(180)
+tk.forward(180)
+tk.right(90)
+tk.forward(180)
+tk.right(90)
+tk.forward(180)
+tk.right(90)
+tk.forward(180)
+tk.home()
+tk.left(180)
+tk.forward(180)
+tk.right(120)
+tk.forward(60)
+tk.left(120)
+
+tk.left(90)
+tk.forward(180)
+tk.right(30)
+tk.forward(60)
+
+
+tk.left(120)
+tk.forward(180)
+tk.left(60)
+tk.forward(60)
+
+
+
+
+
+turtle.done()
---
+++ draw/tria.py
@@ -0,0 +1,13 @@
+import turtle
+tk=turtle.Turtle()
+for i in range(2):
+    tk.right(90)
+    tk.forward(180)
+    tk.right(90)
+    tk.forward(180)
+
+tk.left(120)
+tk.forward(180)
+tk.left(120)
+tk.forward(180)
+turtle.done()
